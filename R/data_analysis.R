472322af066ca9facc353fc9cfba47c30cbf4bee
Mohamad Shakil Hasan <shakilmohamad7@gmail.com>
2017-07-10 10:40:07 -0400

    z_prior adjusted

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -6,14 +6,19 @@
 #' @param alpha Nmeric, significance level of the hypothesis test
 #' @param pvalue a vector of pvalues of the test statistics
 #' @param filter a vector of filter statistics
-#' @param N_current Integer, number of observations in the current data
-#' @param N_prior Integer, number of observations in the prior data
+#' @param N_current Integer vector, number of observations per test in the
+#' current data
+#' @param N_prior Integer vector, number of observations per test in the
+#' prior data
 #' @param tail right-tailed or two-tailed hypothesis test.
 #' default is right-tailed test.
 #' @param max.group maximum number of groups to be used to split the p-values,
 #' default is five. Note that, it is better to keep approximately 1000 p-values
 #' per group.
-#' @param effectType Character of type ("binary","continuous") of effect sizes
+#' @param standarized Character of c("TRUE" or "FALSE") determine whether
+#' standarization is required. Default is FALSE means filter is a vector of
+#' zscore. Thus, standarization will not used.
+#' @param effectType Character of type ("binary" or"continuous") of effect sizes
 #'
 #' @details Perform data analysis for the different methods such as proposed,
 #' bonferroni, Benjamini and Hoghburgh, IHW, and Dorbibian methods.
@@ -39,14 +44,30 @@
 #'          N_prior = m, tail = 2, max.group = 10, effectType = "continuous")
 #'
 #===============================================================================
+# sigma = a vector of standard deviaitons
+# z_prior = prior information for the bayes_weight. In the bayes_weight
+# it is treated as the zscore with left-tailed test. Therefore, standarization
+# is necessary if filter is not the zcore and also neagaitive(-) sign is used
+# to indacate left-tailed test, because all other methods are based on
+# right-tailed test
+#===============================================================================
 data_analysis <- function(alpha, pvalue, filter, N_current, N_prior, tail,
-                          max.group, effectType = c("continuous", "binary"))
+                          max.group, standarized = FALSE,
+                          effectType = c("continuous", "binary"))
 {
     m = length(pvalue)
     sigma <- sqrt(N_current/N_prior)
-    z_prior<- (filter - mean(filter, na.rm = TRUE))/sd(filter, na.rm = TRUE)
+
+    if(standarized == TRUE){
+        mn = mean(filter, na.rm = TRUE)
+        sdv = sd(filter, na.rm = TRUE)
+        z_prior = -(filter - mn)/sdv
+    } else {
+        z_prior = -filter
+    }
+
     mu <- sqrt(N_current/N_prior)*z_prior
-    dbn_wgt <- bayes_weights(mu = -mu, sigma = sigma, q = alpha/m)$w
+    dbn_wgt <- bayes_weights(mu = mu, sigma = sigma, q = alpha/m)$w
 
     # FWER cotrols-------
     pro_bon <- empOPW(pvalue = pvalue, filter = filter, alpha = alpha,
